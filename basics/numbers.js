170282cbb4d585df39ddf10439e0c7181e21a8d7
sebaslh12 <sebaslh12@gmail.com>
2019-03-17 16:34:43 -0500

    Js Basics: numbers Number definition Number Operations Operators precedence

---
+++ basics/numbers.js
@@ -0,0 +1,17 @@
+let x = 11
+let num = (x + 1) * 2 //operator precedence
+
+console.log(num);
+
+let age = 24
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+//Lecture Challenge: Calculate grade percent having the student score and the max score
+
+let studentScore = 41
+let maxScore = 100
+let percent = (studentScore / maxScore) * 100
+
+console.log(percent)
