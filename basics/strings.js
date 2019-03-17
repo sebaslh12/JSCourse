58fd490f0fa596cdd03a7a175a69fc08e44db72f
sebaslh12 <sebaslh12@gmail.com>
2019-03-17 16:19:46 -0500

    Js Basics: strings String definition String Variable Concatenation

---
+++ basics/strings.js
@@ -0,0 +1,13 @@
+let firstName = 'Sebastian'
+let lastName = 'Lozano H'
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
+
+//Lecture Challenge
+
+let city = 'Cali'
+let country = 'Colombia'
+let location = city + ', ' + country
+
+console.log(location)
