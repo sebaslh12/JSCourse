9072e3b676d5632fed2b902cc5f8b0a343af81ac
sebaslh12 <sebaslh12@gmail.com>
2019-03-22 16:06:43 -0500

    Basics: functions Parts of a function Syntax

---
+++ functions/functions-101.js
@@ -0,0 +1,30 @@
+//Function - arguments, code, return value
+
+let greetUser = function () {
+    console.log('Welcome user')
+}
+
+greetUser();
+
+let square = function (num) {
+    let result = num * num
+    return result;
+}
+
+let value = square(3);
+let otherValue = square(10)
+
+console.log(value)
+console.log(otherValue)
+
+//Lecture Challenge: Create a function to convert farenheit to celsius
+
+let farenheitToCelsius = function(farenheitTemp){
+    return (farenheitTemp - 32) * (5 / 9)
+}
+
+let firstConversion = farenheitToCelsius(32)
+let secondConversion = farenheitToCelsius(68)
+
+console.log(firstConversion)
+console.log(secondConversion)
