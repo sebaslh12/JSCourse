a9453412b66919c1dc9d2fc3cf2b5cdfed8980ec
sebaslh12 <sebaslh12@gmail.com>
2019-03-30 15:43:07 -0500

    Expense tracker

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -4,10 +4,35 @@ let myAccount = {
     income: 0
 }
 
-//Objects are passed by reference, it's a pointer to a memory position, changing one of the properties will change in all of the other instances
+// Objects are passed by reference, it's a pointer to a memory position, changing one of the properties will change in all of the other instances
 let addExpense = function (account, amount) {
     account.expenses += amount
 }
 
-addExpense(myAccount, 10)
+// addIncome
+let addIncome = function (account, income) {
+    account.income += income
+}
+
+// resetAccount
+let resetAccount = function (account) {
+    account.income = 0
+    account.expenses = 0
+}
+
+// Account summary, Account for Sebastian has $900. $1000 in income. $100 in expenses.
+
+let getAccountSummary = function (account) {
+    let balance = account.income - account.expenses
+    let summary = `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses`
+    console.log(summary)
+    return summary
+}
+
+addIncome(myAccount, 1000)
+addExpense(myAccount, 50)
+addExpense(myAccount, 50)
+getAccountSummary(myAccount)
+resetAccount(myAccount)
+getAccountSummary(myAccount)
 console.log(myAccount);
