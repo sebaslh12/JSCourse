6fc50d237923ae173ed9a9d06017edeef107a991
sebaslh12 <sebaslh12@gmail.com>
2019-03-31 15:56:14 -0500

    Js in the browser: DOM manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,12 @@
-console.log('This is from a different file')
+// DOM - Document Object Model
+
+// Query selector returns the first occurrence
+/* const p = document.querySelector('p')
+p.remove() */
+
+// Query all returns an array with the matching results
+const ps = document.querySelectorAll('p')
+ps.forEach((p) => {
+    //console.log(p.textContent)
+    p.textContent = '******'
+})
