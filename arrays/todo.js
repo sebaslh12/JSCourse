aae147caebf65b9623a275c552cf4c99d8f340d4
sebaslh12 <sebaslh12@gmail.com>
2019-03-30 19:41:10 -0500

    Basics: Arrays Definition, Syntax, Properties and Access

---
+++ arrays/todo.js
@@ -0,0 +1,7 @@
+const todos = ['Learn Js', 'Learn Node', 'Finish RnR course', 'Leave this country', 'Get a cat']
+
+// Lecture Challenge, create an array with five todos, print how many todos you have, print the first todo and print the second to last todo
+
+console.log(`You have ${todos.length} todos`);
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
