d099560a04d8ab1f3a9f186de32e844b0f173ffa
sebaslh12 <sebaslh12@gmail.com>
2019-03-31 11:56:37 -0500

    Basics: Searching Arrays 2

--- arrays/notes.js
+++ arrays/notes.js
@@ -16,38 +16,58 @@ notes.push('Note 4')
 console.log(notes.shift());
 notes.unshift('First note') */
 
-notes.splice(1, 1) // removes one item starting in position 1
+/* notes.splice(1, 1) // removes one item starting in position 1
 notes.splice(1, 0, 'New second note') // Works as an index insertion
-notes.splice(1, 1, 'New new second note') // Works as an index replacement just like notes[1] = newValuew
+notes.splice(1, 1, 'New new second note') // Works as an index replacement just like notes[1] = newValuew */
 
 console.log(notes.length);
 console.log(notes);
 
 // There is no way to stop or break a forEach()
 // Passing a function to a function is a Callback
-notes.forEach((note, index) => {
+/* notes.forEach((note, index) => {
     console.log(note);
     console.log(index);
-})
+}) */
 
 // Unlike forEach, for loops allows early termination
-for (let count = 2; count >= 0; count--) {
+/* for (let count = 2; count >= 0; count--) {
     console.log(`Counting... ${count}`);
 }
 
 for (let count = 0; count < notes.length; count++) {
     //console.log(notes[count]);
-}
+} */
 
-let someObject = {}
+/* let someObject = {}
 let otherObject = someObject
 console.log(someObject === otherObject) // true, an object is equal only to itself
 console.log({} === {}) // false
 console.log(notes.indexOf({})) //-1, indexOf can't find an object ocurrence inside an array
+ */
 
 // To find an object occurrence we need to use findIndex and pass how the objects should be compared
-const index = notes.findIndex((note) => {
+/* const index = notes.findIndex((note) => {
     return note.title === 'My next job'
 })
+console.log(index) */
 
-console.log(index)
+/* const findNote = function (notes, noteTitle) {
+    const index = notes.findIndex((note) => {
+        console.log(note.title, note)
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+    return notes[index]
+} */
+
+// Find returns the match and no the index
+const findNote = function (notes, noteTitle) {
+    return notes.find((note) => {
+        console.log(note.title, note)
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
+
+// Like objects, arrays are also passed by reference
+const note = findNote(notes, 'office modification')
+console.log(note)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,29 +1,60 @@
-const todos = ['Learn Js', 'Learn Node', 'Finish RnR course', 'Leave this country', 'Get a cat']
+const todos = [
+    {
+        text: 'Learn Js',
+        completed: true
+    },
+    {
+        text: 'Learn Node',
+        completed: true
+    },
+    {
+        text: 'Finish RnR course',
+        completed: false
+    },
+    {
+        text: 'Leave this country',
+        completed: false
+    },
+    {
+        text: 'Get a cat',
+        completed: false
+    }
+]
 
 // Lecture Challenge, create an array with five todos, print how many todos you have, print the first todo and print the second to last todo
-
-console.log(`You have ${todos.length} todos`);
+/* console.log(`You have ${todos.length} todos`);
 console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[todos.length - 2]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`) */
 
 // Lecture Challenge, delete 3rd item, push new item, remove the first item
-
-todos.splice(2, 1)
+/* todos.splice(2, 1)
 todos.push('Conquer the world')
 todos.shift()
 
-console.log(`You have ${todos.length} todos`);
+console.log(`You have ${todos.length} todos`); */
 
 // Lecture challenge, print all of the todos with this format 'index. todo'
-
-todos.forEach((todo,index)=>{
+/* todos.forEach((todo,index)=>{
     const pos = index + 1
     console.log(`${pos}. ${todo}`);
-})
+}) */
 
 // Lecture challenge use for loop and print the same as the forEach loop
-
-for (let index = 0; index < todos.length; index++) {
+/* for (let index = 0; index < todos.length; index++) {
     const pos = index + 1
     console.log(`${pos}. ${todos[index]}`);
+} */
+
+// Lecture challenge, delete todo by text value
+
+const deleteTodo = function (todos, todoText) {
+    const index = todos.findIndex((todo) => {
+        return todo.text.toLowerCase() === todoText.toLowerCase()
+    })
+    if (index != -1) {
+        todos.splice(index, 1)
+    }
 }
+
+deleteTodo(todos, 'Learn js')
+console.log(todos)
