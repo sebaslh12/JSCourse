ad66ee93766dfe899ac04349fadeb359271e34a6
sebaslh12 <sebaslh12@gmail.com>
2019-04-01 14:07:27 -0500

    Js in the browser: Adding elements via DOM

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,4 +1,16 @@
 // DOM - Document Object Model
+const notes = [
+    {
+        title: 'My next trip',
+        body: 'I would like to go to Spain'
+    }, {
+        title: 'My next job',
+        body: 'Study'
+    }, {
+        title: 'Office modification',
+        body: 'Get a new seat'
+    }
+]
 
 // Query selector returns the first occurrence
 /* const p = document.querySelector('p')
@@ -10,3 +22,9 @@ ps.forEach((p) => {
     //console.log(p.textContent)
     p.textContent = '******'
 })
+
+// Add a new element
+const newParagraph = document.createElement('p')
+
+newParagraph.textContent = 'This is a new element from JavaScript'
+document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,8 +1,47 @@
+const todos = [
+    {
+        text: 'Learn Js',
+        completed: true
+    },
+    {
+        text: 'Learn Node',
+        completed: true
+    },
+    {
+        text: 'Finish RnR course',
+        completed: false
+    },
+    {
+        text: 'Leave this country',
+        completed: false
+    },
+    {
+        text: 'Get a cat',
+        completed: false
+    }
+]
 
-const ps = document.querySelectorAll('p')
+/* const ps = document.querySelectorAll('p')
 
 ps.forEach((p) => {
     if (p.textContent.toLowerCase().includes('the')) {
         p.remove()
     }
+}) */
+
+// Lecture challenge, add p per each todo text value. Add a summary that says how many todos you have left
+const body = document.querySelector('body')
+
+const unfinishedTodos = todos.filter((todo) => {
+    return !todo.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${unfinishedTodos.length} todos left`
+body.appendChild(summary)
+
+todos.forEach((todo) => {
+    const todoParagraph = document.createElement('p')
+    todoParagraph.textContent = todo.text
+    body.appendChild(todoParagraph)
 })
