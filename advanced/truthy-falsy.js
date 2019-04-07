b5020235c7befd759169ad94ccb0868de3c0b4a6
sebaslh12 <sebaslh12@gmail.com>
2019-04-07 17:33:09 -0500

    Advanced: truthy and falsy values

---
+++ advanced/truthy-falsy.js
@@ -0,0 +1,13 @@
+const products = []
+const product = products[0]
+// All values are either truthy or falsy
+// Truthy - Values that resolve to true in boolean context
+// False - Values that resolve to false in boolean context
+// Falsy values - false, 0, empty string, null, undefined, NaN
+
+if (product) {
+    console.log('Product found')
+} else {
+    console.log('Product not found');
+
+}
