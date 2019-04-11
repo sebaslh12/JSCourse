32eb60ddcc0a767e5e8ffcecdd84d461e7065e2a
sebaslh12 <sebaslh12@gmail.com>
2019-04-11 17:35:24 -0500

    HTTP errors

--- hangman/app.js
+++ hangman/app.js
@@ -33,10 +33,12 @@ document.addEventListener('keypress', (e) => {
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status == 200) {
         const data = JSON.parse(e.target.responseText)
         console.log(data.puzzle)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place')
     }
 })
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
