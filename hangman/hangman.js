3191e03754bc412f633605e2b17ba10c4465d745
sebaslh12 <sebaslh12@gmail.com>
2019-04-11 10:47:15 -0500

    Using Classes: ES2015

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,53 +1,57 @@
 // Lecture challenge: create a Hangman constructor function
+class Hangman {
+    
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split('');
+        this.guessedLetters = [];
+        this.remainingGuesses = remainingGuesses;
+        this.status = 'playing';
+    }
 
-const Hangman = function (word, remainingGuesses) {
-    this.word = word.toLowerCase().split('')
-    this.guessedLetters = []
-    this.remainingGuesses = remainingGuesses
-    this.status = 'playing'
-}
+    getPuzzle() {
+        let wordPuzzle = this.word.join('');
+        this.word.forEach((letter) => {
+            if (!this.guessedLetters.includes(letter)) {
+                if (letter !== ' ')
+                    wordPuzzle = wordPuzzle.replace(letter, '*');
+            }
+        });
+        return wordPuzzle;
+    }
 
-Hangman.prototype.getPuzzle = function () {
-    let wordPuzzle = this.word.join('')
-    this.word.forEach((letter) => {
-        if (!this.guessedLetters.includes(letter)) {
-            if (letter !== ' ')
-                wordPuzzle = wordPuzzle.replace(letter, '*')
+    makeAGuess(guess) {
+        guess = guess.toLowerCase();
+        const isUnique = !this.guessedLetters.includes(guess);
+        const isBadGuess = !this.word.includes(guess);
+        if (isUnique) {
+            if (this.remainingGuesses)
+                this.guessedLetters.push(guess);
         }
-    })
-    return wordPuzzle
-}
-
-Hangman.prototype.makeAGuess = function (guess) {
-    guess = guess.toLowerCase()
-    const isUnique = !this.guessedLetters.includes(guess)
-    const isBadGuess = !this.word.includes(guess)
-    if (isUnique) {
-        if (this.remainingGuesses)
-            this.guessedLetters.push(guess)
+        if (isUnique && isBadGuess && this.remainingGuesses)
+            this.remainingGuesses--;
+        this.calculateStatus();
     }
-    if (isUnique && isBadGuess && this.remainingGuesses)
-        this.remainingGuesses--
 
-    this.calculateStatus()
-}
-
-Hangman.prototype.calculateStatus = function () {
-    if (!this.remainingGuesses) {
-        this.status = 'failed'
-    } else {
-        const isFinished = this.word.every((letter) => this.guessedLetters.includes(letter))
-        this.status = isFinished ? 'finished' : this.status
+    calculateStatus() {
+        if (!this.remainingGuesses) {
+            this.status = 'failed';
+        }
+        else {
+            const isFinished = this.word.every((letter) => this.guessedLetters.includes(letter));
+            this.status = isFinished ? 'finished' : this.status;
+        }
+        this.getStatusMessage();
     }
-    this.getStatusMessage()
-}
 
-Hangman.prototype.getStatusMessage = function () {
-    if (this.status === 'failed') {
-        return `Nice try! The word was "${this.word.join('')}".`
-    } else if (this.status === 'finished') {
-        return `Great work you guessed the word!`
-    } else {
-        return `Guesses left: ${this.remainingGuesses}`
+    getStatusMessage() {
+        if (this.status === 'failed') {
+            return `Nice try! The word was "${this.word.join('')}".`;
+        }
+        else if (this.status === 'finished') {
+            return `Great work you guessed the word!`;
+        }
+        else {
+            return `Guesses left: ${this.remainingGuesses}`;
+        }
     }
 }
--- oop/person.js
+++ oop/person.js
@@ -1,14 +1,16 @@
 // Functions that can be use after a new operator are constructor functions
 // Prototypal Inheritance: is a form of inheritance it is use to pass the methods across all the instances
-const Person = function (firstName, lastName, age, likes = []) {
+
+// This constructor function it's an old way to do things
+/* const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
     this.likes = likes
-}
+} */
 
 // We set up methods on the prototype property of the constructor function we can share those methods accross all the instances
-Person.prototype.getBio = function () {
+/* Person.prototype.getBio = function () {
     let bio = `${this.firstName} is ${this.age}.`
 
     // Unlike normal functions, Arrow function does not bind this, so this code works
@@ -23,7 +25,37 @@ Person.prototype.setName = function (fullName) {
     const names = fullName.split(' ')
     this.firstName = names[0]
     this.lastName = names[1]
+} */
+
+// After ES2015 class was introduced, it does the same things thant the other definition, prototypal inheritance is still used behind the scenes
+
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
+
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+
+        return bio
+    }
+
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
+
+const myPersonClass = new Person('Sebastian', 'Lozano', 24, ['Learning'])
+console.log(myPersonClass.getBio())
 
 const me = new Person('Sebastian', 'Lozano', 24, ['Videogames', 'Sleeping'])
 me.setName('Alexis Turner')
