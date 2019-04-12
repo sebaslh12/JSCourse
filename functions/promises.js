015edd7fed0aa97dd930f2e2eb15770d88d049fe
sebaslh12 <sebaslh12@gmail.com>
2019-04-12 12:30:13 -0500

    Chaining Promises

--- functions/promises.js
+++ functions/promises.js
@@ -1,32 +1,52 @@
 // Callback
-
-const getDataCallback = (callback) => {
+// With callback nesting is the only way
+const getDataCallback = (num, callback) => {
     setTimeout(() => {
-        callback(undefined, 'This is the data')
+        if (typeof num === 'number')
+            callback(undefined, num * 2)
+        else
+            callback('Number must provided')
     }, 2000)
 }
 
-getDataCallback((err, data) => {
+getDataCallback(2, (err, data) => {
     if (err) {
-
+        console.log(err)
     } else {
-        console.log(data)
+        getDataCallback(data, (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(data)
+            }
+        })
     }
 })
 
-// Promise
+// Promise: Promise pattern uses resolve and reject finish functions, only one of them will execute no matter how many times we execute them. It has a defined semantics that do not depends on changing parameters order and values
 
-const getDataPromise = () => new Promise((resolve, reject) => {
+const getDataPromise = (num) => new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve('This is the promise data')
-        reject('Error')
+        typeof num === 'number' ? resolve(num * 2) : reject('Number must provided')
     }, 2000)
 })
 
-const myPromise = getDataPromise()
-
-myPromise.then((data) => {
-    console.log(data)
+// Nesting promises
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log(data)
+    }, (err) => {
+        console.log(err)
+    })
 }, (error) => {
     console.log(error)
+})
+
+// Chaining promises, the promise inside the handler can be returned so we chain the sucess and the error handlers
+getDataPromise('10').then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    console.log(data)
+}).catch((err) => {
+    console.log(err)
 })
