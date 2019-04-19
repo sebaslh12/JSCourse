5273988d1aef9cfc7b7a1cb4a3ae273fb55eb30b
sebaslh12 <sebaslh12@gmail.com>
2019-04-19 16:55:35 -0500

    Converting Notes App I

---
+++ notes-app/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        index: ['babel-polyfill', './src/index.js'],
+        edit: ['babel-polyfill', './src/edit.js']
+    },
+    output: {
+        // __dirname gives us the absolute path to the project folder
+        // path is a node library to get all the path methods
+        // We have to do this because path needs an absolute path and it has to be compatible with everyone in every os
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: '[name]-bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        publicPath: '/scripts/'
+    },
+    devtool: 'source-map'
+}
